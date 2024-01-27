92522d4d194fefdb4f9c2b9be566ca3e81a4147e
Kevin Fernando <122973372+WhisperGo@users.noreply.github.com>
2024-01-27 13:45:32 +0700

    Kevin

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        echo view('hopeui/partial/header');
+        echo view('hopeui/partial/side_menu');
+        echo view('hopeui/partial/top_menu'); 
+        echo view('hopeui/dashboard/view');
+        echo view('hopeui/partial/footer');
     }
 }
